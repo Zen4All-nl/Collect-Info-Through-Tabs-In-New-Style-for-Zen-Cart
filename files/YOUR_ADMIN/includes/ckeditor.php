76dd2bf206267ef516e4f505611d0757a7e1244b
Zen4All <info@zen4all.nl>
2017-09-17 11:55:37 +0200

    update

--- files/YOUR_ADMIN/includes/ckeditor.php
+++ files/YOUR_ADMIN/includes/ckeditor.php
@@ -17,9 +17,6 @@ foreach ($var as $key)
   $jsLanguageLookupArray .= "  lang[" . $key['id'] . "] = '" . $key['code'] . "';\n";
 }
 ?>
-<!--<script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"><\/script>');</script>
-<script>window.jQuery || document.write('<script src="includes/javascript/jquery-1.12.1.min.js"><\/script>');</script> -->
-<!-- <script src="//cdn.ckeditor.com/4.7.2/standard/ckeditor.js"></script> -->
 <script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>ckeditor/ckeditor.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
@@ -39,24 +36,3 @@ $(document).ready(function() {
   });
 });
 </script>
-<!--<script type="text/javascript">
-  $('.editorHook').each(function() {
-      CKEDITOR.replace(this.id,
-        {
-          coreStyles_underline : { element : 'u' },
-          width : 760,
-          language: lang[index]
-        });
-    });
-</script>-->
-<!--<script type="text/javascript">
-$(function(){
-    $('.editorHook').each(function(e){
-        CKEDITOR.replace( this.id,
-        {
-          coreStyles_underline : { element : 'u' },
-          width : 760
-        });
-    });
-});
-</script>-->
