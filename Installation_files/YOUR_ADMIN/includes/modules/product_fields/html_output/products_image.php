f973d5dbdf8098c8724a36687ddae7ec750b1d98
Zen4All <info@zen4all.nl>
2019-02-26 17:58:09 +0100

    update
    fix typo $productInfo['product_image'] should be $productInfo['products_image']

--- Installation_files/YOUR_ADMIN/includes/modules/product_fields/html_output/products_image.php
+++ Installation_files/YOUR_ADMIN/includes/modules/product_fields/html_output/products_image.php
@@ -5,22 +5,22 @@
       <div class="panel-body">
         <div class="col-sm-8">
           <div id="mainImageThumb" data-toggle="modal" data-target="#imagePreviewModal" role="button">
-              <?php if ($productInfo['product_image']['value'] != '') { ?>
-                <?php echo zen_image(DIR_WS_CATALOG_IMAGES . $productInfo['product_image']['value'], '', SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, 'class="img-thumbnail" id="mainImage"'); ?>
+              <?php if ($productInfo['products_image']['value'] != '') { ?>
+                <?php echo zen_image(DIR_WS_CATALOG_IMAGES . $productInfo['products_image']['value'], '', SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, 'class="img-thumbnail" id="mainImage"'); ?>
               <br/>
               <?php echo TEXT_CLICK_TO_ENLARGE; ?>
             <?php } else { ?>
               <?php echo NONE; ?>
             <?php } ?>
           </div>
-          <?php echo zen_draw_hidden_field('products_image', $productInfo['product_image']['value'], 'id="mainProductImage"'); ?>
-          <?php echo zen_draw_hidden_field('products_previous_image', $productInfo['product_image']['value']); ?>
+          <?php echo zen_draw_hidden_field('products_image', $productInfo['products_image']['value'], 'id="mainProductImage"'); ?>
+          <?php echo zen_draw_hidden_field('products_previous_image', $productInfo['products_image']['value']); ?>
           <div id="mainImagePath">
-            <?php echo ($productInfo['product_image']['value'] != '' ? $productInfo['product_image']['value'] : NONE); ?></div>
+            <?php echo ($productInfo['products_image']['value'] != '' ? $productInfo['products_image']['value'] : NONE); ?></div>
         </div>
         <div class="col-sm-4">
           <div role="group">
-              <?php if ($productInfo['product_image']['value'] != '') { ?>
+              <?php if ($productInfo['products_image']['value'] != '') { ?>
               <button type="button" id="button-edit-main-image" class="btn btn-primary" data-original-title="<?php echo TEXT_CHANGE_IMAGE; ?>" data-toggle="modal" data-target="#mainImageEditModal"><i class="fa fa-pencil" aria-hidden="true"></i></button>
               <button type="button" id="button-delete-main-image-1" class="btn btn-danger" data-original-title="<?php ?>" data-toggle="modal" data-target="#mainImageDeleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
             <?php } else { ?>
@@ -34,11 +34,11 @@
 </div>
 <!-- /* BOF future code */ -->
 <?php
-if ($productInfo['product_image']['value'] != '' && $productInfo['products_id']['value'] != '') {
-  $additionalImages = getAdditionalImages($productInfo['products_id']['value'], $productInfo['product_image']['value']);
+if ($productInfo['products_image']['value'] != '' && $productInfo['products_id']['value'] != '') {
+  $additionalImages = getAdditionalImages($productInfo['products_id']['value'], $productInfo['products_image']['value']);
 } // if products_image
 ?>
-<div class="panel panel-primary" id="additionalImages"<?php echo (($productInfo['product_image']['value'] != '' && $productInfo['products_id']['value'] != '') ? '' : ' style="display:none"') ?>>
+<div class="panel panel-primary" id="additionalImages"<?php echo (($productInfo['products_image']['value'] != '' && $productInfo['products_id']['value'] != '') ? '' : ' style="display:none"') ?>>
   <div class="panel-heading"><?php echo TEXT_ADDITIONAL_IMAGES; ?></div>
   <div class="panel-body">
       <?php
