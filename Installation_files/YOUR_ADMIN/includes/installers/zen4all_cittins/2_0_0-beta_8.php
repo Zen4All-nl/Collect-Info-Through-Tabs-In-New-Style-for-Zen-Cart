df20dd6bc4f2fb113656334ace2a04b949d49ee4
Zen4All <info@zen4all.nl>
2019-12-18 17:05:38 +0100

    Add missing version bump file for beta 8

---
+++ Installation_files/YOUR_ADMIN/includes/installers/zen4all_cittins/2_0_0-beta_8.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * 2_0_0-beta_8.php
+ *
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: Zen4All
+ */
